09099751d12bff57142a029c4c0f37c9a45ef118
zzyzy <leezhenzhi@gmail.com>
2015-10-08 02:38:10 +0800

    Added Google login

--- app/src/main/java/com/paperfly/instantjio/MainActivity.java
+++ app/src/main/java/com/paperfly/instantjio/MainActivity.java
@@ -1,5 +1,9 @@
 package com.paperfly.instantjio;
 
+import android.accounts.Account;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,25 +15,125 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
 import com.paperfly.instantjio.contacts.ContactsFragment;
 import com.paperfly.instantjio.events.EventsFragment;
 import com.paperfly.instantjio.groups.GroupsFragment;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener  {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
+
+    private Firebase ref;
+
+    private Firebase.AuthStateListener authStateListener;
+
+    private Firebase.AuthResultHandler authResultHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initFirebase();
+        initGoogleAPI();
         initToolbar();
         initViewPagerAndTabs();
         initFAB();
+    }
+
+    private void initFirebase() {
+        ref = new Firebase("https://popping-inferno-4928.firebaseio.com/");
+
+        authStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                if (authData != null) {
+                    findViewById(R.id.container_main).setVisibility(View.VISIBLE);
+                    findViewById(R.id.appbar).setVisibility(View.VISIBLE);
+                    findViewById(R.id.container_login).setVisibility(View.GONE);
+                } else {
+                    // user is not logged in
+                    findViewById(R.id.container_login).setVisibility(View.VISIBLE);
+                    findViewById(R.id.container_main).setVisibility(View.GONE);
+                    findViewById(R.id.appbar).setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), "Logged out from Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        authResultHandler = new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                // Authenticated successfully with payload authData
+                Toast.makeText(getApplicationContext(), "Firebase authenticated", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                // Something went wrong :(
+                switch (firebaseError.getCode()) {
+                    case FirebaseError.USER_DOES_NOT_EXIST:
+                        // handle a non existing user
+                        break;
+                    case FirebaseError.INVALID_PASSWORD:
+                        // handle an invalid password
+                        break;
+                    default:
+                        // handle other errors
+                        break;
+                }
+            }
+        };
+    }
+
+    private void initGoogleAPI() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.EMAIL))
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSignInClicked();
+            }
+        });
     }
 
     private void initToolbar() {
@@ -46,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
         mSectionsPagerAdapter.addFragment(ContactsFragment.newInstance(), "Contacts");
 
         // Set up the ViewPager with the sections adapter.
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.container_main);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
@@ -64,28 +168,157 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ref.addAuthStateListener(authStateListener);
+        if (!mGoogleApiClient.isConnected())
+            mGoogleApiClient.connect();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ref.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mGoogleApiClient.isConnected())
+            mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+//                showErrorDialog(connectionResult);
+                Toast.makeText(getApplicationContext(), connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Show the signed-out UI
+//            showSignedOutUI();
+            Toast.makeText(getApplicationContext(), "Failed to sign in to Google", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        if (!mGoogleApiClient.isConnected() && !mGoogleApiClient.isConnecting()) {
+            mGoogleApiClient.connect();
+
+            // Show a message to the user that we are signing in.
+            Toast.makeText(getApplicationContext(), "Signed in to Google", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        Log.d(TAG, "onConnected:" + bundle);
+        mShouldResolve = false;
+        GetIdTokenTask task = new GetIdTokenTask();
+        task.execute();
+
+        // Show the signed-in UI
+//        showSignedInUI();
+        Toast.makeText(getApplicationContext(), "Signed in to Google", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // ignore
+    }
+
+    private void onSignOutClicked() {
+        // Clear the default account so that GoogleApiClient will not automatically
+        // connect in the future.
+        ref.unauth();
+
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
+            Toast.makeText(getApplicationContext(), "Signed out from Google", Toast.LENGTH_SHORT).show();
+        }
+//        showSignedOutUI();
+    }
+
+    private class GetIdTokenTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String accountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
+            Account account = new Account(accountName, GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+            String scopes = "oauth2:" + Scopes.PROFILE + " " + Scopes.EMAIL;
+            try {
+                return GoogleAuthUtil.getToken(getApplicationContext(), account, scopes);
+            } catch (UserRecoverableAuthException e) {
+                if (!mIsResolving) {
+                    mIsResolving = true;
+                    startActivityForResult(e.getIntent(), RC_SIGN_IN);
+                }
+                return null;
+            } catch (IOException e) {
+                Log.e(TAG, "Error retrieving ID token.", e);
+                return null;
+            } catch (GoogleAuthException e) {
+                Log.e(TAG, "Error retrieving ID token.", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(TAG, "ID token: " + result);
+            if (result != null) {
+                // Successfully retrieved ID Token
+                // ...
+                if (ref.getAuth() == null)
+                    ref.authWithOAuthToken("google", result, authResultHandler);
+            } else {
+                // There was some error getting the ID Token
+                // ...
+            }
+        }
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
---
+++ app/src/main/java/com/paperfly/instantjio/MainApplication.java
@@ -0,0 +1,16 @@
+package com.paperfly.instantjio;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Initialize Firebase with the application context. This must happen before the client is used.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(getApplicationContext());
+    }
+}
