359510823e4ec22b7ccdf49e326a0fc64d0d54cb
zzyzy <leezhenzhi@gmail.com>
2015-10-20 11:29:36 +0800

    Moved layout from activity to fragment to promote reusability

--- app/src/main/java/com/paperfly/instantjio/event/EventTemplateCreateActivity.java
+++ app/src/main/java/com/paperfly/instantjio/event/EventTemplateCreateActivity.java
@@ -2,6 +2,7 @@ package com.paperfly.instantjio.event;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.paperfly.instantjio.R;
 
@@ -11,5 +12,20 @@ public class EventTemplateCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_template_create);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
