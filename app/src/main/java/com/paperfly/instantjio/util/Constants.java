a2959991343b55da04bbbc07ef4785a1bf125bfc
zzyzy <leezhenzhi@gmail.com>
2015-10-22 00:34:18 +0800

    Added new constants

--- app/src/main/java/com/paperfly/instantjio/util/Constants.java
+++ app/src/main/java/com/paperfly/instantjio/util/Constants.java
@@ -4,4 +4,9 @@ public class Constants {
     public static final String TAG = Constants.class.getCanonicalName();
     public static final String EVENT_OBJECT = TAG + ".EVENT_OBJECT";
     public static final String EVENT_KEY = TAG + ".EVENT_KEY";
+    public static final String GROUP_OBJECT = TAG + ".GROUP_OBJECT";
+    public static final String GROUP_KEY = TAG + ".GROUP_KEY";
+    public static final String CHOOSING_MODE = TAG + ".CHOOSING_MODE";
+    public static final String CHOSEN_GROUPS = TAG + ".CHOSEN_GROUPS";
+    public static final String CHOSEN_CONTACTS = TAG + ".CHOSEN_CONTACTS";
 }
